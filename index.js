b3845c3afc1019a5a11e80cb08053fc1162e6d5d
aninuraini <128433050+aninuraini@users.noreply.github.com>
2023-03-21 15:03:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+var hasil = 150;
+var panjang = 9;
+var lebar = 15;
+
+// Apakah benar hasil = panjang x lebar ?
+
+const result = panjang * lebar;
+
+if (result === hasil) {
+  console.log('Benar');
+} else {
+  console.log('Salah');
+}
 
